cf4eb228f2d2866e1ce13f621e9f5da396a9a6a6
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-01-20 22:11:07 -0800

    Login page UI

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import "./login.scss";
+
+function Login() {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Sky Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+
+          <button>Sign in</button>
+        </form>
+        <p>Don't have an account? Register</p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
