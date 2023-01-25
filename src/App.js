d2585995062b83d390ba47982198f902c4bf415b
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-01-24 22:12:39 -0800

    React router dom added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/notFound/NotFound";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <NotFound/>
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+
+]);
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
