d0500f4072fee1f5c7c4d587221370502fc258f1
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-02-10 09:58:21 -0800

    Render Sidebar and Chat into homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Chat from '../../components/chat/Chat'
+import Sidebar from '../../components/sidebar/Sidebar'
+import './home.scss'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+    <div className="container">
+      <Sidebar/>
+      <Chat/>
+    </div>  
+    </div>
   )
 }
 
