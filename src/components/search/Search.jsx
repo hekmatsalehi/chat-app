de6e6733d20102624851d91d1518648d43dd1f52
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-02-20 12:39:19 -0800

    Search UI

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,8 +1,22 @@
+import "./search.scss";
 
 function Search() {
-  return (
-    <div>Search</div>
-  )
+    return (
+        <div className="search">
+            <div className="searchForm">
+                <input type="text" placeholder="Find a user" />
+            </div>
+            <div className="userChat">
+                <img
+                    src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                    alt=""
+                />
+                <div className="userChatInfo">
+                    <span>Tom</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Search
+export default Search;
