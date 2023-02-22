5c1925fa5ae5183a6de5ea469a2c04452f2ebb09
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-02-22 12:39:45 -0800

    Chats UI

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,9 +1,40 @@
-import './chats.scss'
+import "./chats.scss";
 
 function Chats() {
   return (
-    <div className="chats">Chats</div>
-  )
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Tom</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Tom</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Tom</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Chats
+export default Chats;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,14 @@
 import Navbar from '../navbar/Navbar'
+import Search from '../search/Search'
+import Chats from '../chats/Chats'
 import './sidebar.scss'
 
 function Sidebar() {
   return (
     <div className="sidebar">
         <Navbar/>
+        <Search/>
+        <Chats/>
     </div>
   )
 }
