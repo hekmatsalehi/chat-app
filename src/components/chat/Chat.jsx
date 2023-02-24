e5d2b5cbb4b9c3d434e54c93540ff54edf903737
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-02-24 11:48:26 -0800

    Chat component UI

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,25 @@
-import './chat.scss'
+import { IoMdVideocam, IoMdPersonAdd } from "react-icons/io";
+import { BsThreeDots } from "react-icons/bs";
+import Messages from "../messages/Messages";
+import Input from "../input/Input"
+import "./chat.scss";
 
 function Chat() {
-  return (
-    <div className="chat">Chat</div>
-  )
+    return (
+        <div className="chat">
+            <div className="chatInfo">
+                <span>Tom</span>
+                <div className="chatIcons">
+                    <IoMdVideocam className="icon" />
+                    <IoMdPersonAdd className="icon" />
+                    <BsThreeDots className="icon" />
+                </div>
+            </div>
+
+            <Messages />
+            <Input/>
+        </div>
+    );
 }
 
-export default Chat
+export default Chat;
