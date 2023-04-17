b217774c814a7e34d5afa192b5a2028ebd1f6f59
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-04-17 09:55:57 -0700

    Set up Firebase store

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,27 +1,98 @@
 import { BsImageFill } from "react-icons/bs";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../../firebase"
+import { useState } from "react";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
+
 import "./register.scss";
 
 function Register() {
-  return (
-    <div className="formContainer">
-      <div className="formWrapper">
-        <span className="logo">Sky Chat</span>
-        <span className="title">Register</span>
-        <form>
-          <input type="text" placeholder="display name" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <label htmlFor="file">
-            <BsImageFill className="avatar" />
-            Add an avatar
-          </label>
-          <input style={{ display: "none" }} type="file" id="file" />
-          <button>Sign up</button>
-        </form>
-        <p>Do you have an account? Login</p>
-      </div>
-    </div>
-  );
+
+    const [error, setError] = useState("");
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+        
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password)
+            console.log(response.user)
+            const storageRef = ref(storage, displayName); // displayName is as a fileName
+            
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            
+            uploadTask.on('state_changed', 
+              (snapshot) => {
+                // Observe state change events such as progress, pause, and resume
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                  case 'paused':
+                    console.log('Upload is paused');
+                    break;
+                  case 'running':
+                    console.log('Upload is running');
+                    break;
+
+                  default: 
+                    console.log(snapshot.state)
+                    break;
+                }
+              }, 
+              (error) => {
+                setError(error.message)
+              }, 
+              () => {
+                // Handle successful uploads on complete
+                getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+                  await updateProfile(response.user, {
+                    displayName,
+                    photoURL: downloadURL
+
+                  })
+
+                  await setDoc(doc(db, "users", response.user.uid), {
+                      uid: response.user.uid,
+                      displayName,
+                      email,
+                      photoURL: downloadURL
+                    });
+                });
+              },
+            );
+
+
+        } catch(error) {
+            setError(error.message)
+        }
+    };
+
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">Sky Chat</span>
+                <span className="title">Register</span>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="display name" />
+                    <input type="email" placeholder="email" />
+                    <input type="password" placeholder="password" />
+                    <label htmlFor="file">
+                        <BsImageFill className="avatar" />
+                        Add an avatar
+                    </label>
+                    <input style={{ display: "none" }} type="file" id="file" />
+                    {error && <span className="errorMsg" >{error}</span>}
+                    <button>Sign up</button>
+                </form>
+                <p>Do you have an account? Login</p>
+            </div>
+        </div>
+    );
 }
 
 export default Register;
