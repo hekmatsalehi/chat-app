1d4f75cd0c512dc0be8760823bdc2996e77e6718
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-04-27 12:59:37 -0700

    Created AuthContext

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/notFound/NotFound";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-    errorElement: <NotFound/>
-  },
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router}/>
-  );
+    const { currentUser } = useContext(AuthContext);
+    console.log(currentUser);
+    return <RouterProvider router={router} />;
 }
 
 export default App;
