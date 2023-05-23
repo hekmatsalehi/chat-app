f59f575bc01333c33497cba799fdb1c75409c8d0
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-05-23 12:47:26 -0700

    Render userChat on sidebar

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
-import "./chats.scss";
 import { AuthContext } from "../../context/AuthContext";
+import "./chats.scss";
 
 function Chats() {
     const [chats, setChats] = useState([]);
@@ -11,56 +11,32 @@ function Chats() {
 
     useEffect(() => {
         const getChats = () => {
-          const unsub = onSnapshot(
-              doc(db, "userChats", currentUser.uid),
-              (doc) => {
-                  setChats(doc.data());
-              }
-          );
-  
-          return () => {
-              unsub();
-          };
+            const unsub = onSnapshot(
+                doc(db, "userChats", currentUser.uid),
+                (doc) => {
+                    setChats(doc.data());
+                }
+            );
+
+            return () => {
+                unsub();
+            };
         };
 
-        currentUser.uid && getChats()
-        
+        currentUser.uid && getChats();
     }, [currentUser.uid]);
-
-    console.log(Object.entries(chats));
 
     return (
         <div className="chats">
-            <div className="userChat">
-                <img
-                    src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                />
-                <div className="userChatInfo">
-                    <span>Tom</span>
-                    <p>Hello</p>
+            {Object.entries(chats)?.map((chat) => (
+                <div className="userChat" key={chat[0]}>
+                    <img src={chat[1].userInfo.photoURL} alt="" />
+                    <div className="userChatInfo">
+                        <span>{chat[1].userInfo.displayName}</span>
+                        <p>{chat[1].userInfo.lastMessage?.text}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="userChat">
-                <img
-                    src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                />
-                <div className="userChatInfo">
-                    <span>Tom</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="userChat">
-                <img
-                    src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                />
-                <div className="userChatInfo">
-                    <span>Tom</span>
-                    <p>Hello</p>
-                </div>
-            </div>
+            ))}
         </div>
     );
 }
