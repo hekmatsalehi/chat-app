30693afce7a0486f97d92ec58357fd0d2a4f881b
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2023-06-03 16:01:16 -0700

    Show messages on chat box

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import "./message.scss";
@@ -7,22 +7,38 @@ function Message({ message }) {
     const { currentUser } = useContext(AuthContext);
     const { data } = useContext(ChatContext);
 
+    const chatRef = useRef(null);
+
+    console.log("--- message", message)
+
+    useEffect(() => {
+        if (chatRef.current) {
+            chatRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [message]);
+
     return (
-        <div className="message owner">
-            {/* <div className="messageInfo">
+        <div
+            className={`message ${
+                message.senderId === currentUser.uid && "owner"
+            }`}
+            ref={chatRef}
+        >
+            <div className="messageInfo">
                 <img
-                    src="https://images.unsplash.com/photo-1661261656149-879b82aaad59?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    src={
+                        message.senderId === currentUser.uid
+                            ? currentUser.photoURL
+                            : data.user.photoURL
+                    }
                     alt=""
                 />
                 <span>just now</span>
             </div>
             <div className="messageContent">
-                <p>Hello</p>
-                <img
-                    src="https://images.unsplash.com/photo-1678644897769-8eeaa24b1980?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIzfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                />
-            </div> */}
+                <p>{message.text}</p>
+                {message.img && <img src={message.img} alt="" />}
+            </div>
         </div>
     );
 }
